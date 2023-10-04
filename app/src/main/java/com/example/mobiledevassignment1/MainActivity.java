1760fc0d2e58baefe74de69c0b010ca081d19627
Mithusan <mithusan.arul11@gmail.com>
2023-10-04 11:29:05 -0400

    stuff

--- app/src/main/java/com/example/mobiledevassignment1/MainActivity.java
+++ app/src/main/java/com/example/mobiledevassignment1/MainActivity.java
@@ -11,27 +11,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Initialize Variables
     int principal;
     double interest;
     int term;
     double monthlyPayment;
-
     EditText principalInput;
     EditText interestInput;
     EditText termInput;
 
-    public boolean checkIsEmpty(int[] ids) {
-        boolean isEmpty = false;
+    //Function to check if TextFields are Valid, Returns Boolean
+    public boolean checkIsValid(int[] ids) {
+        boolean isValid = true;//Variable Starts Valid until Further Checks
 
+        //Loops for amount of items in ids array
         for(int id: ids) {
             EditText input = (EditText)findViewById(id);
 
+            //If Current TextField is Empty
             if(TextUtils.isEmpty(input.getText().toString())) {
-                isEmpty = true;
+                Toast.makeText(MainActivity.this, "Empty fields not allowed!", Toast.LENGTH_SHORT).show();//Error Msg
+                isValid = false;
+                break;//Prevent Error - If Value is Empty next Check cannot occur
+            }
+
+            //If Value of Current TextField is <= 0
+            if (Double.valueOf(input.getText().toString()) <= 0){
+                Toast.makeText(MainActivity.this, "Values Entered Must be Greater than 0!", Toast.LENGTH_SHORT).show();//Error Msg
+                isValid = false;
             }
         }
-
-        return isEmpty;
+        return isValid;
     }
 
     @Override
@@ -39,31 +49,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView output = (TextView) findViewById(R.id.text_output);
-        Button calc = (Button) findViewById(R.id.btn_calculate);
-
+        //Put Id of Input Field in an Array to be used later in ValidChecks
         int[] ids = new int[]{
                 R.id.input_principalAmount,
                 R.id.input_interestRate,
                 R.id.input_term
         };
 
+        //Initialize Elements
+        TextView output = (TextView) findViewById(R.id.text_output);
+        Button calc = (Button) findViewById(R.id.btn_calculate);
+
+        //Grab User Input
         principalInput = (EditText) findViewById(R.id.input_principalAmount);
         interestInput = (EditText) findViewById(R.id.input_interestRate);
         termInput = (EditText) findViewById(R.id.input_term);
 
+        //Listens for when 'Calculate' Button is Clicked
         calc.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                if(!checkIsEmpty(ids)){
+                if(checkIsValid(ids)){ //If Valid
+                    //Converts String Inputs into Integers
                     principal = Integer.valueOf(principalInput.getText().toString());
                     interest = ((Double.valueOf(interestInput.getText().toString())) / 100) / 12;
                     term = Integer.valueOf(termInput.getText().toString());
 
+                    //Compute
                     monthlyPayment = (principal * interest * (Math.pow(1 + interest, term))) / ((Math.pow(1 + interest, term)) - 1);
+
+                    //Output
                     output.setText("Monthly Payment: $" + String.format("%.2f", monthlyPayment));
-                } else {
-                    Toast.makeText(MainActivity.this, "Empty field not allowed!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
